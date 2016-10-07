2760b969cd1b37858d05393a99be78d1d78f8dc1
mumingv <mumingv@163.com>
2016-10-07 21:19:01 +0800

    add file

---
+++ shell/192.Word.Frequency/solution_3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# 单词数量统计
+
+declare -A words_array
+file='words.txt'
+
+# 使用关联数组保存单词统计计数
+function word_count {
+    # 统计
+    while read line; do
+        array=(${line})
+        for word in ${array[*]}; do
+            let words_array[${word}]++
+        done
+    done < ${file}
+    # 输出
+    for word in ${!words_array[*]}; do
+        echo ${word} ${words_array[${word}]}
+    done
+}
+
+word_count | sort -k 2 -r
