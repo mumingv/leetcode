9bb5411ba9d9b2cd70e82a2fd48479c191f6d229
mumingv <mumingv@163.com>
2017-03-20 10:06:47 +0800

    python

---
+++ python/001.Two.Sum/solution.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(nums) <= 1:
+            return False
+        buff_dict = {}  # key存放期待配对的值，value存放当前值的索引
+        for i in range(len(nums)):
+            if nums[i] in buff_dict:
+                return [buff_dict[nums[i]], i]
+            else:
+                buff_dict[target - nums[i]] = i
+
+obj = Solution()
+nums = [2, 7, 11, 15]
+target = 13
+print obj.twoSum(nums, target)
---
+++ python/002.Add.Two.Numbers/solution.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+"""
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        carry = 0
+        root = n = ListNode(0)
+        while l1 or l2 or carry:
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+            carry, val = divmod(v1+v2+carry, 10)
+            n.next = ListNode(val)
+            n = n.next
+        return root.next
+
+list1 = [2,4,3]
+list2 = [5,6,4]        
+obj = Solution()
+obj.addTwoNumbers(list1, list2)
+ 
